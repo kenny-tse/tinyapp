41ce36c0a8914eefd2b9cc06a8840db33c98e574
kenny-tse <kennytse91@gmail.com>
2021-09-27 21:47:09 +0000

    added urls_new template and handling post request for entering new url

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 3000; // default port 8080
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
@@ -9,18 +12,27 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-// app.get("/", (req, res) => {
-//   res.send("Hello!");
-// });
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send(generateRandomString());         // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/hello", (req, res) => {
   const templateVars = { greeting: 'Hello World!' };
   res.render("hello_world", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -32,3 +44,18 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+function generateRandomString() {
+
+  let randomString = "";
+
+  for (let i = 0; i < 5; i++) {
+
+    if (Math.floor((Math.random() * 10) + 1) % 2 === 0) {
+      randomString = randomString + String.fromCharCode(Math.floor(Math.random() * (122 - 97) + 97));
+    } else {
+      randomString = randomString + String.fromCharCode(Math.floor(Math.random() * (57 - 48) + 48));
+    }
+  }
+
+  return randomString;
+}
